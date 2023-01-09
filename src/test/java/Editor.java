9d1939f9c9fe1778019f24c06e9bbae0c17f5e72
Smrity44 <Smritybdny@gmail.com>
2023-01-09 16:57:08 -0500

    adding

--- src/test/java/Editor.java
+++ src/test/java/Editor.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Editor {
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class Editor extends Base{
+
+
+        @Test
+        public void Editor() {
+            String url = "https://the-internet.herokuapp.com/";
+            driver.get(url);
+            driver.findElement(By.cssSelector("body p")).getText();
+
+
+
+        }
 }
--- src/test/java/MultipleWindows.java
+++ src/test/java/MultipleWindows.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class MultipleWindows {
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class MultipleWindows extends Base {
+
+    @Test
+    public void multipleWindows() throws InterruptedException {
+        String url ="https://the-internet.herokuapp.com/";
+        driver.get(url);
+        driver.findElement(By.linkText("Multiple Windows")).click();
+        driver.findElement(By.linkText("Click Here")).click();
+        driver.findElement(By.className("example")).click();
+
+
+        Thread.sleep(10000);
+
+
+    }
 }
